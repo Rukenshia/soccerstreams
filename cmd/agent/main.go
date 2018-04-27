b5476bf910e698c7b671a16346a9eeea4e999ab2
Jan Christophersen <jan@ruken.pw>
2018-04-27 18:37:45 +0200

    trigger build (updated graw)

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Rukenshia/graw/reddit"
 	"github.com/Rukenshia/soccerstreams/cmd/agent/metrics"
 	"github.com/Rukenshia/soccerstreams/pkg/soccerstreams"
+
 	raven "github.com/getsentry/raven-go"
 	log "github.com/sirupsen/logrus"
 )
